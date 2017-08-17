18dd2605edead556538978b86fc04e3475b475dd
Hac Phan <hac@madkudu.com>
2017-08-17 09:53:21 -0700

    Style

--- test/indicative.js
+++ test/indicative.js
@@ -20,29 +20,29 @@ const isFalse = function (data, field, message, args, get) {
 indicative.extend('isFalse', isFalse, 'value should be false')
 
 const rules = {
-  "type": "equals:identify",
-  "traits.email": "required|email",
-  "traits.mk_is_personal": "required|is_false",
-  "traits.mk_is_edu": "required|is_false",
-  "traits.mk_is_disposable": "required|is_false",
-  "traits.mk_is_student": "required|is_false"
+  'type': 'equals:identify',
+  'traits.email': 'required|email',
+  'traits.mk_is_personal': 'required|is_false',
+  'traits.mk_is_edu': 'required|is_false',
+  'traits.mk_is_disposable': 'required|is_false',
+  'traits.mk_is_student': 'required|is_false'
 }
 
 const data = {
-  "userId": "lila@42debut.com",
-  "traits": {
-    "email": "lila@42debut.com",
-    "mk_is_student": false,
-    "mk_is_edu": false,
-    "mk_is_personal": false,
-    "mk_is_disposable": false,
-    "mk_company_name": "42 Technologies",
-    "mk_customer_fit_segment": "low",
-    "mk_first_name": "Lila",
-    "mk_last_name": "Cantor"
+  'userId': 'lila@42debut.com',
+  'traits': {
+    'email': 'lila@42debut.com',
+    'mk_is_student': false,
+    'mk_is_edu': false,
+    'mk_is_personal': false,
+    'mk_is_disposable': false,
+    'mk_company_name': '42 Technologies',
+    'mk_customer_fit_segment': 'low',
+    'mk_first_name': 'Lila',
+    'mk_last_name': 'Cantor'
   },
-  "context": {
-    "active": false
+  'context': {
+    'active': false
   }
 }
 
